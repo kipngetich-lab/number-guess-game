ce5d0dc6b455ac70816cf0d4272fa305c66bd3d8
Kipngetich Josphat <joskipngetich07@gmail.com>
2025-09-13 22:48:23 +0300

    build configuration

--- backend/server.js
+++ backend/server.js
@@ -30,9 +30,9 @@ app.use('/api/game', gameRoutes);
 
 //production
 if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(currentDir,"/client/dist")));
+  app.use(express.static(path.join(currentDir,"/frontend/dist")));
   app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(currentDir,"client","dist","index.html"));
+    res.sendFile(path.resolve(currentDir,"frontend","dist","index.html"));
   })
 }
 
